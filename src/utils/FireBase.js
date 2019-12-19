a33c366c67abf2e02d168482b2101dc9cb9a0e24
Jorge Puerta <jorge.puerta00@gmail.com>
2019-12-18 20:06:50 -0500

    Update FireBase.js

--- src/utils/FireBase.js
+++ src/utils/FireBase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCczhUT92YJuSP-uc-3IRzDGlMMX3NqU3I",
-    authDomain: "appmovilcmadrid.firebaseapp.com",
-    databaseURL: "https://appmovilcmadrid.firebaseio.com",
-    projectId: "appmovilcmadrid",
-    storageBucket: "appmovilcmadrid.appspot.com",
-    messagingSenderId: "796279763948",
-    appId: "1:796279763948:web:30182eac0c100e4f5406c9",
-    measurementId: "G-HDZP0GL7EB"
+    apiKey: "********************************************",
+    authDomain: "****************************************",
+    databaseURL: "***************************************",
+    projectId: "*****************************************",
+    storageBucket: "*************************************",
+    messagingSenderId: "*********************************",
+    appId: "*********************************************",
+    measurementId: "*************************************",
   };
 
   export const  firebaseApp = firebase.initializeApp(firebaseConfig);
